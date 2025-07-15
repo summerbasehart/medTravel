78beea1ebba9790c970c3b90638053d52ac09d23
Summer GB <47164799+summerbasehart@users.noreply.github.com>
2025-07-15 15:28:50 -0700

    insurances

--- src/app/intl-ins/intl-ins.component.ts
+++ src/app/intl-ins/intl-ins.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AdminComponent } from "../admin/admin.component";
 
 @Component({
   selector: 'app-intl-ins',
   standalone: true,
-  imports: [],
+  imports: [AdminComponent],
   templateUrl: './intl-ins.component.html',
   styleUrl: './intl-ins.component.css'
 })
